c9e96dcdc1166a0238441f14ca2952ccd272a445
alvin.yeoh <alvin.yeoh@gumi.sg>
2021-07-04 19:56:37 +0800

    Added dotenv

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	fmt.Println("Hello World")
+	fmt.Println("Env value is:", os.Getenv("MY_ENV_VAL"))
 }
